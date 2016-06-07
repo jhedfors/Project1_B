3f844c71ad421b71141dd50e7fff3f317dd5e5de
Jeff Hedfors <git.j@hedfors.net>
2016-06-07 16:49:25 -0700

    fix upload

--- queries.sql
+++ queries.sql
@@ -1,45 +1,67 @@
-/*
-
-
-
-*/
-
+select * from quotes;
 select * from users;
+select * from favorites;
 
-SELECT * FROM users WHERE email = 'jeff@hedfors.net';
+INSERT INTO quotes (user_id, speaker, quote, created_at, modified_at) VALUES('1','Douglas Adams','So long, and thanks for all the fish!', NOW(),NOW());
+INSERT INTO quotes (user_id, speaker, quote, created_at, modified_at) VALUES('1','Douasdfdams','Sasdfasdffor all the fish!', NOW(),NOW());
+INSERT INTO quotes (user_id, speaker, quote, created_at, modified_at) VALUES('1','Douasds Adams','So long, andasdfasdf all the fish!', NOW(),NOW());
+INSERT INTO quotes (user_id, speaker, quote, created_at, modified_at) VALUES('1','Douglas asdf','So as, andasdfthanks for all the fish!', NOW(),NOW());
 
-insert into users (name, alias, email, password, dob, created_at, modified_at) values('Jayden Hedfors','jayden','jayden@hedfors.net','a642a77abd7d4f51bf9226ceaf891fcbb5b299b8','1967/11/30',NOW(),NOW());
-
--- poke a person
-SELECT * FROM pokes;
--- poke a person
-SELECT count  FROM pokes
-WHERE pokee_id = 6 AND poker_id = 1;
-
--- poke a person
-insert into pokes (poker_id, pokee_id, count, created_at, modified_at) values(8,8,0,NOW(),NOW());
-
-SELECT poker_id, r.name as poker_name, pokee_id, e.name as pokee_name, count from pokes
-left join users r on r.id = pokes.poker_id
-left join users e on e.id = pokes.pokee_id;
-
-SELECT SUM(count) as pokes_recieved, pokee_id FROM pokes
-WHERE pokee_id = 6;
-
-SELECT users.id, name, alias, email, SUM(count) as pokes_recieved FROM users
-LEFT JOIN pokes ON users.id = pokee_id;
-
-SELECT poker_id, users.name as pokee_name, pokee_id  from pokes
-left join users on users.id = pokee_id;
-
-select users.name from users where id=5;
+SELECT quote_id from favorites
+where user_id = 1;
 
 
 
+-- all quotes
+SELECT quotes.id as quote_id, users.id as poster_id, users.alias as alias, speaker, quote from quotes
+LEFT JOIN users ON users.id = quotes.user_id
+LEFT JOIN favorites on favorites.quote_id = quotes.id;
 
-insert into users (name) values ("jeff");
-select * from users;
+-- all quotes by user
+SELECT users.alias as alias, speaker, quote from quotes
+LEFT JOIN users ON users.id = quotes.user_id
+WHERE users.id = 1;
 
-insert into pokes (poker_id, pokee_id) values (5,6);
-select * from pokes;
+--  in favorites	
+SELECT quotes.id as quote_id, users.id as poster_id, users.alias as alias, speaker, quote from quotes
+LEFT JOIN users ON users.id = quotes.user_id
+LEFT JOIN favorites on favorites.quote_id = quotes.id
+WHERE favorites.user_id = 1 ;
 
+-- not in favorites
+SELECT quotes.id as quote_id, users.id as poster_id, users.alias as alias, speaker, quote from quotes
+LEFT JOIN users ON users.id = quotes.user_id
+LEFT JOIN favorites on favorites.quote_id = quotes.id
+WHERE NOT quotes.id in
+(SELECT quotes.id from quotes
+WHERE favorites.user_id=1);
+
+
+select * from favorites;
+
+ -- add favorite
+insert into favorites (user_id, quote_id) VALUES (1,4);
+
+ -- delete
+ DELETE from favorites WHERE user_id = 1 AND quote_id = 4;
+
+ 
+
+
+
+SELECT * FROM trips
+left join users
+on users.id = trip_creator_id
+where not trips.id in (SELECT trips.id FROM trips
+left JOIN user_trips
+ON user_trips.trips_id = trips.id
+left JOIN users
+ON user_trips.users_id = users.id
+where users.id = 1);
+
+
+
+
+SELECT quotes.id as quote_id, quotes.user_id, favorites. id as favorite_id, users.alias as alias, speaker, quote, quotes.created_at  from quotes
+			LEFT JOIN users ON users.id = quotes.user_id
+			LEFT JOIN favorites on favorites.quote_id = quotes.id
